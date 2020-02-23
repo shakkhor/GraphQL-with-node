adb0fc0db5ab14ed1856fd88be741962a042775c
shakkhor <abdullahshakkhor@gmail.com>
2020-02-24 01:49:07 +0600

    graphql is set up and go

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express')
 const expressGraphQL = require('express-graphql')
 const {
     GraphQLSchema,
-    GraphQLObjectType
+    GraphQLObjectType,
+    GraphQLString
 } = require('graphql')
 
 app = express()
+const schema = new GraphQLSchema({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: ()=>({
+            message:{
+                type:GraphQLString,
+                resolve: ()=>  'Hello World!'
+            }
+        })
+    })
+})
+
 app.use('/graphql', expressGraphQL({
     graphiql:true,
+    schema:schema
     
 }))
 app.listen(5000., ()=> console.log('Server Running'))
